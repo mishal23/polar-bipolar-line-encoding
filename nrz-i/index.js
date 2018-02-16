a915af509e5dbb6b68389d55979de78e6decac23
Mishal Shah <shahmishal1998@gmail.com>
2018-02-16 13:42:29 +0530

    nrz-l, nrz-i done

--- nrz-i/index.js
+++ nrz-i/index.js
@@ -4,3 +4,104 @@ $("#about_nrz-i").typed({
     ],
     typeSpeed: 0,
   });
+
+$(document).ready(function () {
+	$('#submit').click(function(){
+		var data_bit = $('#data_bit').val();
+		var voltage = $("#voltage").val();
+		if(data_bit==="" && voltage==="")
+		{
+			 Materialize.toast('Please enter data bits and voltage', 1000)
+		}
+		else if(data_bit==="")
+		{
+			Materialize.toast('Please enter data bits', 1000)	
+		}
+		else if(voltage==="")
+		{
+			Materialize.toast('Please enter voltage', 1000)	
+		}
+		else
+		{
+			console.log(data_bit);
+			console.log(voltage);	
+
+			arr_databit = data_bit.toString();
+			
+			var x_axis=[];
+			var y_axis = [];
+			var i=0;
+			var k=0;
+			console.log(arr_databit);
+			if(arr_databit[0]=="0")
+			{
+				x_axis[k] = k;
+				y_axis[k] = 1*voltage;
+			}
+			else
+			{
+				x_axis[k] = k;
+				y_axis[k] = -1*voltage;	
+			}
+			k++;
+			for(var i=0;i<=arr_databit.length-1;i++)
+			{	
+				if(i==0)
+				{
+					console.log("0 mein ghussa");
+					if(arr_databit[0]=="0")
+					{
+						console.log("0 mila");
+						x_axis[k] = k;
+						y_axis[k] = 1*voltage;
+					}
+					else
+					{
+						x_axis[k] = k;
+						y_axis[k] = -1*voltage;	
+					}
+				}
+				else
+				{
+					if(arr_databit[i]=="0")
+					{	
+						x_axis[k] = k;
+						y_axis[k] = y_axis[k-1];
+					}
+					else
+					{
+						x_axis[k] = k;
+						y_axis[k] = -1*y_axis[k-1];	
+					}
+				}
+				k++;
+			}
+
+			console.log(x_axis);
+			console.log(y_axis);
+
+			var trace4 = {
+			  x: x_axis, 
+			  y: y_axis, 
+			  mode: 'lines+markers', 
+			  name: 'vh', 
+			  line: {shape: 'vh'}, 
+			  type: 'scatter'
+			};
+
+
+			var data = [trace4];
+
+			var layout = {legend: {
+			    y: 0, 
+			    traceorder: 'reversed', 
+			    font: {size: 16}, 
+			    yref: 'paper'
+			}};
+
+			Plotly.newPlot('myDiv', data, layout);
+
+		}
+		
+	});
+})
--- nrz-l/index.js
+++ nrz-l/index.js
@@ -1,6 +1,96 @@
+var string = "";
+string+="In NRZ-L the level of voltage determines the value of the bit.<br>"
+string+="Rules:<br>"
+string+="0 bit is represented by +V<br>"
+string+="1 bit is represented by -V"
+
+// Typing Content
 $("#about_nrz-l").typed({
     strings: [
-      "Here is all about NRZ-L",
+      string
     ],
     typeSpeed: 0,
   });
+
+
+
+$(document).ready(function () {
+	$('#submit').click(function(){
+		var data_bit = $('#data_bit').val();
+		var voltage = $("#voltage").val();
+		if(data_bit==="" && voltage==="")
+		{
+			 Materialize.toast('Please enter data bits and voltage', 1000)
+		}
+		else if(data_bit==="")
+		{
+			Materialize.toast('Please enter data bits', 1000)	
+		}
+		else if(voltage==="")
+		{
+			Materialize.toast('Please enter voltage', 1000)	
+		}
+		else
+		{
+			console.log(data_bit);
+			console.log(voltage);	
+			arr_databit = data_bit.toString();
+			console.log(arr_databit);
+			var x_axis=[];
+			var y_axis = [];
+			var i=0;
+			var k=0;
+			if(arr_databit[0]==0)
+			{
+				x_axis[k] = k;
+				y_axis[k] = 1*voltage;
+			}
+			else
+			{
+				x_axis[k] = k;
+				y_axis[k] = -1*voltage;	
+			}
+			k++;
+			for(var i=0;i<arr_databit.length;i++)
+			{	
+				if(arr_databit[i]==1)
+				{	
+					x_axis[k] = k;
+					y_axis[k] = -1*voltage;
+				}
+				else
+				{
+					x_axis[k] = k;
+					y_axis[k] = 1*voltage;	
+				}
+				k++;
+			}
+
+			console.log(x_axis);
+			console.log(y_axis);
+
+			var trace4 = {
+			  x: x_axis, 
+			  y: y_axis, 
+			  mode: 'lines+markers', 
+			  name: 'vh', 
+			  line: {shape: 'vh'}, 
+			  type: 'scatter'
+			};
+
+
+			var data = [trace4];
+
+			var layout = {legend: {
+			    y: 0, 
+			    traceorder: 'reversed', 
+			    font: {size: 16}, 
+			    yref: 'paper'
+			}};
+
+			Plotly.newPlot('myDiv', data, layout);
+
+		}
+		
+	});
+})
