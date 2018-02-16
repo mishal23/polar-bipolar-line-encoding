8c891a57b0ad8a7a9de5d581275ad0a4ca3b27b8
SamyakJ2512 <36363430+SamyakJ2512@users.noreply.github.com>
2018-02-16 22:10:29 +0530

    Add files via upload

--- manchester/index.js
+++ manchester/index.js
@@ -1,6 +1,106 @@
+var string = "";
+string+="It is the combination of RZ and NRZ-L.<br>" 
+string+="The duration of bit is divided into 2 halves it remains at one level in first half and other level in second half.<br>"
+
+
+// Typing Content
 $("#about_manchester").typed({
     strings: [
-      "Here is all about Manchester",
+      string
     ],
     typeSpeed: 0,
   });
+
+
+
+$(document).ready(function () {
+	$('#submit').click(function(){
+		var data_bit = $('#data_bit').val();
+		var voltage = $("#voltage").val();
+		if(data_bit==="" && voltage==="")
+		{
+			 Materialize.toast('Please enter data bits and voltage', 1000)
+		}
+		else if(data_bit==="")
+		{
+			Materialize.toast('Please enter data bits', 1000)	
+		}
+		else if(voltage==="")
+		{
+			Materialize.toast('Please enter voltage', 1000)	
+		}
+		else
+		{
+			console.log(data_bit);
+			console.log(voltage);	
+			arr_databit = data_bit.toString();
+			console.log(arr_databit);
+			var x_axis=[];
+			var y_axis = [];
+			var i=0;
+			var k=0;
+			if(arr_databit[0]=="0")
+			{
+				x_axis[k] = k;
+				y_axis[k] = 1*voltage;
+				k++;
+			}
+			else
+			{
+				x_axis[k] = k;
+				y_axis[k] = -1*voltage;	
+				k++;
+				
+			}
+			
+			for(var i=0;i<arr_databit.length;i++)
+			{	
+				if(arr_databit[i]=="1")
+				{	
+				x_axis[k] = k;
+				y_axis[k] = -1*voltage;	
+				k++;
+				x_axis[k] = k;
+				y_axis[k] = 1*voltage;
+				k++;
+				}
+				else
+				{
+				x_axis[k] = k;
+				y_axis[k] = 1*voltage;
+				k++;
+				x_axis[k]=k;
+				y_axis[k]=-1*voltage;
+				k++;
+				}
+				
+			}
+
+			console.log(x_axis);
+			console.log(y_axis);
+
+			var trace4 = {
+			  x: x_axis, 
+			  y: y_axis, 
+			  mode: 'lines+markers', 
+			  name: 'vh', 
+			  line: {shape: 'vh'}, 
+			  type: 'scatter'
+			};
+
+
+			var data = [trace4];
+
+			var layout = {legend: {
+			    y: 0, 
+			    traceorder: 'reversed', 
+			    font: {size: 16}, 
+			    yref: 'paper'
+			}};
+
+			Plotly.newPlot('myDiv', data, layout);
+
+		}
+		
+	});
+})
